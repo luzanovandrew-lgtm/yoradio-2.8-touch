14f80f0bf5840d4b90389c45b6b8c2082d03d070
a.luzanov <a.luzanov@pixonic.com>
2026-07-03 10:55:16 +0300

    Add turquoise LCD theme and weather icons

---
+++ yoRadio/mytheme.h
@@ -0,0 +1,51 @@
+#ifndef mytheme_h
+#define mytheme_h
+
+// Monochrome LCD style with turquoise backlight.
+// Inverted from the previous dark theme: bright background, dark foreground.
+
+#define COLOR_BACKGROUND          170, 255, 244
+
+#define COLOR_STATION_NAME          6,  44,  52
+#define COLOR_STATION_BG          102, 214, 204
+#define COLOR_STATION_FILL        102, 214, 204
+
+#define COLOR_SNG_TITLE_1          10,  74,  82
+#define COLOR_SNG_TITLE_2          24, 112, 120
+#define COLOR_WEATHER              76, 186, 190
+
+#define COLOR_VU_MAX                0,  86,  96
+#define COLOR_VU_MID               10, 138, 146
+#define COLOR_VU_MIN               52, 194, 182
+#define COLOR_VU_FRAME            170, 255, 244
+
+#define COLOR_CLOCK                 0,  96, 104
+#define COLOR_CLOCK_BG            154, 244, 232
+#define COLOR_SECONDS              14, 128, 136
+#define COLOR_DAY_OF_W              0,  96, 104
+#define COLOR_DATE                 12, 120, 128
+
+#define COLOR_HEAP                126, 220, 210
+#define COLOR_BUFFER               12, 118, 126
+#define COLOR_IP                   18, 124, 132
+#define COLOR_VOLUME_VALUE         18, 124, 132
+#define COLOR_RSSI                 18, 124, 132
+#define COLOR_BITRATE               0, 100, 108
+
+#define COLOR_VOLBAR_OUT            0, 126, 128
+#define COLOR_VOLBAR_IN            52, 214, 202
+
+#define COLOR_DIGITS                0,  70,  78
+#define COLOR_DIVIDER              16, 148, 146
+
+#define COLOR_PL_CURRENT            0,  52,  60
+#define COLOR_PL_CURRENT_BG       146, 240, 228
+#define COLOR_PL_CURRENT_FILL      88, 212, 200
+
+#define COLOR_PLAYLIST_0            0,  88,  96
+#define COLOR_PLAYLIST_1            0, 108, 116
+#define COLOR_PLAYLIST_2           10, 132, 140
+#define COLOR_PLAYLIST_3           28, 158, 160
+#define COLOR_PLAYLIST_4           56, 188, 182
+
+#endif
